b41192b6fadfd4034b94e01b18e662393c85a037
Oscar Cabanillas <oskbanillas@gmail.com>
2021-04-20 07:59:12 -0300

    Se agrego el maquetado en la pg AgregarProducto

--- src/components/productos/AgregarProducto.js
+++ src/components/productos/AgregarProducto.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const AgregarProducto = () => {
     return (
-        <div>
-            <h1>Pagina para agregar un producto</h1>
-        </div>
+        <section className="container">
+            <h1 className="text-center">Agregar Nuevo Producto</h1>
+            <Form className="my-4">
+                <Form.Group controlId="formBasictext">
+                    <Form.Label>Nombre del producto *</Form.Label>
+                    <Form.Control type="text" placeholder="Ej: Café con leche" />
+                </Form.Group>
+                <Form.Group controlId="formBasicPrice">
+                    <Form.Label>Precio *</Form.Label>
+                    <Form.Control type="password" placeholder="Ej: $50" />
+                </Form.Group>
+                <Form.Group controlId="formBasicCheckbox" className="text-center">
+                    <h3>Categoria</h3>
+                    {['radio'].map((type) => (
+                        <div key={`inline-${type}`} className="mb-3">
+                            <Form.Check inline label="Bebida caliente" name="group1" type={type} id={`inline-${type}-1`} />
+                            <Form.Check inline label="Bebida fria" name="group1" type={type} id={`inline-${type}-2`} />         
+                            <Form.Check inline label="Sandwich" name="group1" type={type} id={`inline-${type}-3`} />         
+                            <Form.Check inline label="Dulce" name="group1" type={type} id={`inline-${type}-4`} />         
+                            <Form.Check inline label="Salado" name="group1" type={type} id={`inline-${type}-5`} />         
+                        </div>
+                    ))}
+                </Form.Group>
+                <Button variant="danger" size="lg" type="submit" block>
+                    Agregar producto
+        </Button>
+            </Form>
+        </section>
     );
 };
 
