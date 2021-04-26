25de89b90d3916a9bf30c186e37ef7353996d6ab
Oscar Cabanillas <oskbanillas@gmail.com>
2021-04-26 13:15:58 -0300

    condicion inicial

--- src/components/productos/ListarProducto.js
+++ src/components/productos/ListarProducto.js
@@ -3,11 +3,11 @@ import { Container, ListGroup } from "react-bootstrap";
 import ItemProducto from "./ItemProducto";
 
 const ListarProducto = (props) => {
-    console.log(props);
+  console.log(props);
   return (
     <Container>
       <h1 className="my-5 text-center">Listar Productos</h1>
-      <ListGroup className='my-5'>
+      <ListGroup className="my-5">        
         <ItemProducto></ItemProducto>
       </ListGroup>
     </Container>
